fcb2db068dfaedda3e89484216fb01fceb6e3509
max <68344959+codewmax@users.noreply.github.com>
2024-04-19 13:58:11 +0200

    Update style.css and popup.html

--- main/content.js
+++ main/content.js
@@ -1,25 +1,12 @@
-chrome.storage.local.get(['keyBinding', 'outlineStyle', 'elements', 'elementColors'], function (result) {
+chrome.storage.local.get(['keyBinding', 'outlineStyle', 'elements', 'elementColors', 'outlineWidth'], function (result) {
       var keyBinding = result.keyBinding || 'b';
       var outlineStyle = result.outlineStyle || 'dashed';
       var elements = result.elements || ['div', 'p'];
       var elementColors = result.elementColors || {}; // Retrieve the element colors
-    
-      var elementsWithBorder = [];
-      var elementColorddd = {
-            'DIV': 'blue',
-            'P': 'green',
-            'H1': 'red',
-            'H2': 'orange',
-            'H3': 'purple',
-            'H4': 'brown',
-            'H5': 'pink',
-            'H6': 'grey',
-            'SPAN': 'cyan',
-            'A': 'magenta',
-            'IMG': 'yellow',
-            'BUTTON': 'black'
-      };
-    
+      var outlineWidth = result.outlineWidth || '1px';
+
+      var elementsWithOutline = [];
+
       document.addEventListener('keydown', function (event) {
             // Check if the key combination Ctrl + keyBinding was pressed
             if (event.ctrlKey && event.key === keyBinding) {
@@ -27,48 +14,49 @@ chrome.storage.local.get(['keyBinding', 'outlineStyle', 'elements', 'elementColo
                   elements.forEach(function (elementType) {
                         // If a color for this element type does not exist, use black as default
                         var color = elementColors[elementType] || 'black';
-    
+
                         // Get all elements of this type
                         var elementsOfType = document.getElementsByTagName(elementType);
-    
                         // Loop through all elements of this type
                         Array.from(elementsOfType).forEach(function (element) {
                               // Draw an outline around the element with the color
-                              element.style.outline = '1px ' + outlineStyle + ' ' + color;
-    
+                              element.style.outline = outlineWidth + 'px ' + outlineStyle + ' ' + color;
+
                               // Add the element to the array of elements with an outline
-                              elementsWithBorder.push(element);
+                              elementsWithOutline.push(element);
                         });
                   });
             }
       });
-    
+
       document.addEventListener('keyup', function (event) {
             // Check if the key combination Ctrl + keyBinding was released
             if (event.key === keyBinding) {
                   // Loop through the elements that had an outline added
-                  elementsWithBorder.forEach(function (element) {
+                  elementsWithOutline.forEach(function (element) {
                         // Remove the outline from the element
                         element.style.outline = '';
                   });
-    
+
                   // Clear the array of elements with an outline
-                  elementsWithBorder = [];
+                  elementsWithOutline = [];
             }
       });
-    
-      chrome.storage.onChanged.addListener(function(changes, namespace) {
-        for (var key in changes) {
-          var storageChange = changes[key];
-          if (key === 'keyBinding') {
-            keyBinding = storageChange.newValue;
-          } else if (key === 'outlineStyle') {
-            outlineStyle = storageChange.newValue;
-          } else if (key === 'elements') {
-            elements = storageChange.newValue;
-          } else if (key === 'elementColors') { // Update elementColors when it changes
-            elementColors = storageChange.newValue;
-          }
-        }
+
+      chrome.storage.onChanged.addListener(function (changes, namespace) {
+            for (var key in changes) {
+                  var storageChange = changes[key];
+                  if (key === 'keyBinding') {
+                        keyBinding = storageChange.newValue;
+                  } else if (key === 'outlineStyle') {
+                        outlineStyle = storageChange.newValue;
+                  } else if (key === 'elements') {
+                        elements = storageChange.newValue;
+                  } else if (key === 'elementColors') { // Update elementColors when it changes
+                        elementColors = storageChange.newValue;
+                  } else if (key === 'outlineWidth') { // Add this line
+                        outlineWidth = storageChange.newValue; // Add 'px' to the new value
+                  }
+            }
       });
-    });
+});
--- main/popup.js
+++ main/popup.js
@@ -1,15 +1,65 @@
-// Default colors object
-var defaultColors = {
-  div: '#000000', // black
-  p: '#000000', // black
-  // Add more elements and their default colors here
-};
+$(document).ready(function() {
+  const container = $('#elementsContainer');
+  const elements = [
+    { id: 'div', color: '#FF6633' },
+    { id: 'p', color: '#FFB399' },
+    { id: 'span', color: '#FF33FF' },
+    { id: 'a', color: '#FFFF99' },
+    { id: 'img', color: '#00B3E6' },
+    { id: 'button', color: '#E6B333' },
+    { id: 'h1', color: '#3366E6' },
+    { id: 'h2', color: '#999966' },
+    { id: 'h3', color: '#99FF99' },
+    { id: 'h4', color: '#B34D4D' },
+    { id: 'h5', color: '#80B300' },
+    { id: 'h6', color: '#809900' },
+    { id: 'form', color: '#E6B3B3' },
+    { id: 'ul', color: '#6680B3' },
+    { id: 'ol', color: '#66991A' },
+    { id: 'li', color: '#FF99E6' },
+    { id: 'table', color: '#CCFF1A' },
+    { id: 'tr', color: '#FF1A66' },
+    { id: 'th', color: '#E6331A' },
+    { id: 'td', color: '#33FFCC' },
+    { id: 'input', color: '#66994D' },
+    { id: 'iframe', color: '#B366CC' },
+    { id: 'label', color: '#4D8000' },
+    { id: 'b', color: '#B33300' },
+    { id: 'i', color: '#CC80CC' },
+    { id: 'u', color: '#66664D' },
+    { id: 'nav', color: '#991AFF' },
+    { id: 'section', color: '#E666FF' },
+    { id: 'article', color: '#4DB3FF' },
+    { id: 'header', color: '#1AB399' },
+    { id: 'footer', color: '#E666B3' },
+    { id: 's', color: '#33991A' },
+    { id: 'sup', color: '#CC9999' },
+    { id: 'sub', color: '#66E64D' },
+    { id: 'strong', color: '#4D80CC' },
+    { id: 'em', color: '#9900B3' },
+    { id: 'q', color: '#E64D66' },
+    { id: 'blockquote', color: '#4DB380' },
+    { id: 'select', color: '#FF4D4D' },
+    { id: 'link', color: '#99E6E6' },
+  ];
+
+
+  elements.forEach(element => {
+    container.append(`
+      <div class="element-container">
+        <input type="checkbox" id="${element.id}" name="${element.id}">
+        <label for="${element.id}">${element.id.toUpperCase()}</label>
+        <input type="color" id="${element.id}Color" value="${element.color}">
+      </div>
+    `);
+  });
 
 
 
-// Function to load the options
+
+  // Function to load the options
 function loadOptions() {
-  chrome.storage.local.get(['keyBinding', 'outlineStyle', 'elements', 'elementColors'], function(result) {
+  chrome.storage.local.get(['keyBinding', 'outlineStyle', 'elements', 'elementColors', 'outlineWidth'], function(result) {
     if (result.keyBinding) {
       $('#key-binding').val(result.keyBinding);
     }
@@ -27,6 +77,9 @@ function loadOptions() {
         $('#' + element + 'Color').val(color); // Set the color picker value for the element
       }
     }
+    if (result.outlineWidth) {
+      $('#outline-width').val(result.outlineWidth);
+    }
   });
 }
 
@@ -38,6 +91,7 @@ var keyBindingInput = $('#key-binding');
 var outlineStyleInput = $('#outline-style');
 var checkboxInputs = $('input[type=checkbox]');
 var colorInputs = $('input[type=color]');
+var outlineWidthInput = $('#outline-width');
 
 // Function to save the options
 function saveOptions() {
@@ -45,6 +99,9 @@ function saveOptions() {
   var outlineStyle = outlineStyleInput.val();
   var elements = checkboxInputs.filter(':checked').map(function() { return this.id; }).get();
   var elementColors = {}; // New object to store element colors
+  var outlineWidth = outlineWidthInput.val();
+
+  console.log(outlineWidth)
 
   // Loop through each checkbox and save its corresponding color
   checkboxInputs.each(function() {
@@ -53,7 +110,6 @@ function saveOptions() {
     elementColors[element] = color;
   });
 
-  console.log(checkboxInputs);
 
   // Save the options to local storage
   console.log('Saving element colors:', elementColors);
@@ -61,7 +117,8 @@ function saveOptions() {
     keyBinding: keyBinding,
     outlineStyle: outlineStyle,
     elements: elements,
-    elementColors: elementColors // Save the element colors
+    elementColors: elementColors, // Save the element colors
+    outlineWidth: outlineWidth
   });
 }
 
@@ -70,3 +127,8 @@ keyBindingInput.on('change', saveOptions);
 outlineStyleInput.on('change', saveOptions);
 checkboxInputs.on('change', saveOptions);
 colorInputs.on('change', saveOptions);
+outlineWidthInput.on('change', saveOptions);
+});
+
+
+
