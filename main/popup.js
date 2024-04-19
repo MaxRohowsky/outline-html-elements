98e8e40a2fe1a3a0c61afa6311dacaea2b8fcb24
max <68344959+codewmax@users.noreply.github.com>
2024-04-19 17:44:33 +0200

    Update style.css and popup.html

--- main/popup.js
+++ main/popup.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const container = $('#elementsContainer');
   const elements = [
     { id: 'div', color: '#FF6633' },
@@ -45,10 +45,11 @@ $(document).ready(function() {
 
 
   elements.forEach(element => {
+
     container.append(`
       <div class="element-container">
         <input type="checkbox" id="${element.id}" name="${element.id}">
-        <label for="${element.id}">${element.id.toUpperCase()}</label>
+        <label for="${element.id}">${element.id.toUpperCase()} </label>
         <input type="color" id="${element.id}Color" value="${element.color}">
       </div>
     `);
@@ -58,76 +59,85 @@ $(document).ready(function() {
 
 
   // Function to load the options
-function loadOptions() {
-  chrome.storage.local.get(['keyBinding', 'outlineStyle', 'elements', 'elementColors', 'outlineWidth'], function(result) {
-    if (result.keyBinding) {
-      $('#key-binding').val(result.keyBinding);
-    }
-    if (result.outlineStyle) {
-      $('#outline-style').val(result.outlineStyle);
-    }
-    if (result.elements) {
-      result.elements.forEach(function(element) {
-        $('#' + element).prop('checked', true);
-      });
-    }
-    if (result.elementColors) { // Check if elementColors is available
-      for (var element in result.elementColors) { // Loop through each element in elementColors
-        var color = result.elementColors[element]; // Get the color for the element
-        $('#' + element + 'Color').val(color); // Set the color picker value for the element
+  function loadOptions() {
+    chrome.storage.local.get(['keyBinding', 'outlineStyle', 'elements', 'elementColors', 'outlineWidth'], function (result) {
+      if (result.keyBinding) {
+        $('#key-binding').val(result.keyBinding);
       }
-    }
-    if (result.outlineWidth) {
-      $('#outline-width').val(result.outlineWidth);
-    }
+      if (result.outlineStyle) {
+        $('#outline-style').val(result.outlineStyle);
+      }
+      if (result.elements) {
+        result.elements.forEach(function (element) {
+          $('#' + element).prop('checked', true);
+        });
+      }
+      if (result.elementColors) { // Check if elementColors is available
+        for (var element in result.elementColors) { // Loop through each element in elementColors
+          var color = result.elementColors[element]; // Get the color for the element
+          $('#' + element + 'Color').val(color); // Set the color picker value for the element
+        }
+      }
+      if (result.outlineWidth) {
+        $('#outline-width').val(result.outlineWidth);
+      }
+    });
+  }
+
+  // Call loadOptions when the popup is opened
+  $(document).ready(loadOptions);
+
+  // Get the input elements
+  var keyBindingInput = $('#key-binding');
+  var outlineStyleInput = $('#outline-style');
+  var checkboxInputs = $('input[type=checkbox]');
+  var colorInputs = $('input[type=color]');
+  var outlineWidthInput = $('#outline-width');
+  var selectAllButton = $('#selectAll');
+  var unselectAllButton = $('#unselectAll');
+
+  // Function to save the options
+  function saveOptions() {
+    var keyBinding = keyBindingInput.val();
+    var outlineStyle = outlineStyleInput.val();
+    var elements = checkboxInputs.filter(':checked').map(function () { return this.id; }).get();
+    var elementColors = {}; // New object to store element colors
+    var outlineWidth = outlineWidthInput.val();
+
+
+    // Loop through each checkbox and save its corresponding color
+    checkboxInputs.each(function () {
+      var element = this.id;
+      var color = $('#' + element + 'Color').val();
+      elementColors[element] = color;
+    });
+
+
+    // Save the options to local storage
+    console.log('Saving element colors:', elementColors);
+    chrome.storage.local.set({
+      keyBinding: keyBinding,
+      outlineStyle: outlineStyle,
+      elements: elements,
+      elementColors: elementColors, // Save the element colors
+      outlineWidth: outlineWidth
+    });
+  }
+
+  // Add event listeners to the input elements
+  keyBindingInput.on('change', saveOptions);
+  outlineStyleInput.on('change', saveOptions);
+  checkboxInputs.on('change', saveOptions);
+  colorInputs.on('change', saveOptions);
+  outlineWidthInput.on('change', saveOptions);
+  selectAllButton.on('click', function () {
+    checkboxInputs.prop('checked', true);
+    saveOptions(); // Save options after selecting all checkboxes
   });
-}
-
-// Call loadOptions when the popup is opened
-$(document).ready(loadOptions);
-
-// Get the input elements
-var keyBindingInput = $('#key-binding');
-var outlineStyleInput = $('#outline-style');
-var checkboxInputs = $('input[type=checkbox]');
-var colorInputs = $('input[type=color]');
-var outlineWidthInput = $('#outline-width');
-
-// Function to save the options
-function saveOptions() {
-  var keyBinding = keyBindingInput.val();
-  var outlineStyle = outlineStyleInput.val();
-  var elements = checkboxInputs.filter(':checked').map(function() { return this.id; }).get();
-  var elementColors = {}; // New object to store element colors
-  var outlineWidth = outlineWidthInput.val();
-
-  console.log(outlineWidth)
-
-  // Loop through each checkbox and save its corresponding color
-  checkboxInputs.each(function() {
-    var element = this.id;
-    var color = $('#' + element + 'Color').val();
-    elementColors[element] = color;
+  unselectAllButton.on('click', function () {
+    checkboxInputs.prop('checked', false);
+    saveOptions(); // Save options after unselecting all checkboxes
   });
-
-
-  // Save the options to local storage
-  console.log('Saving element colors:', elementColors);
-  chrome.storage.local.set({
-    keyBinding: keyBinding,
-    outlineStyle: outlineStyle,
-    elements: elements,
-    elementColors: elementColors, // Save the element colors
-    outlineWidth: outlineWidth
-  });
-}
-
-// Add event listeners to the input elements
-keyBindingInput.on('change', saveOptions);
-outlineStyleInput.on('change', saveOptions);
-checkboxInputs.on('change', saveOptions);
-colorInputs.on('change', saveOptions);
-outlineWidthInput.on('change', saveOptions);
 });
 
 
